81136ceb5b726e1a459e9ce85da46f71ae2f698a
leejihyung <sg01257@naver.com>
2016-12-11 13:28:19 +0900

    modified timer

--- threads/thread.h
+++ threads/thread.h
@@ -91,6 +91,9 @@ struct thread
     int priority;                       /* Priority. */
     struct list_elem allelem;           /* List element for all threads list. */
 
+    /* Save wakeup time for sleeping thread */
+    int64_t wakeup_tick;  
+
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
 
